9e35f144539ceab59f1e3cd88dcea47fb2da0bca
Yuri Andrade <dayannadir.2011@gmail.com>
2024-10-31 08:02:05 -0300

    commit aula

---
+++ pasta_oo/aula 30 10/codigo_livre.py
@@ -0,0 +1,18 @@
+while True:
+    print("""Você entro na calculadora feita pelo Yuri.
+Nós temos as seguintes opções:
+          
+1) Soma
+          
+2) Subtração
+          
+3) Divisão
+          
+4) Multiplicação
+          
+5) Potência 
+
+6) Raiz
+""")
+    
+    opcao_input = float(input("Digite a opção que deseja usar. "))

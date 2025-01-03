05e23c58567f83487b846ba09edcf3e9370696a6
y123yuri <dayannadir.2011@gmail.com>
2025-01-02 21:19:01 -0300

    troca de main para projeto oo

--- projeto oo/main.py
+++ projeto oo/main.py
@@ -1,9 +1,8 @@
 import sys
 import os
-1
-# 2
+
 #  a pasta "classes" ao caminho de busca de módulos
-diretorio_classes = os.path.join(os.path.dirname(__file__), 'projeto oo\classes')
+diretorio_classes = os.path.join(os.path.dirname(__file__), 'classes')
 sys.path.append(diretorio_classes)
 
 from arquivo_main import *
